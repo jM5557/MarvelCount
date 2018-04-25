af1958e8bac25fe9fcf74050a9f6ddf322ef7d3a
Javier Minchala <javierminchala@Javiers-Air.home>
2018-04-25 14:42:19 -0400

    First production commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import vueScrollTo from 'vue-scrollto';
+
+import NavigationBar from './components/NavigationBar.vue';
+
+import App from './App.vue';
+import Comparison from './components/Comparison.vue';
+import Evolution from './components/Evolution.vue';
+
+import FooterComponent from './components/FooterComponent.vue';
+
+import { renderComponentIfElementExists } from './lib/helpers.js';
+import './scss/style.scss';
+
+Vue.use(vueScrollTo);
+
+
+renderComponentIfElementExists ('app-nav', NavigationBar);
+
+renderComponentIfElementExists ('app-home', App);
+renderComponentIfElementExists ('app-comparison', Comparison);
+renderComponentIfElementExists ('app-evolution', Evolution);
+
+renderComponentIfElementExists ('app-footer', FooterComponent);
